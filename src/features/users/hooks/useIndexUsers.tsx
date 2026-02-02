54e5a0e9d0d3821caa8b4ae6187d25556e8836c7
anthony laverde <anthonylaverde@MacBook-Pro-de-anthony.local>
2026-02-02 07:48:04 -0300

    en este commit actualizamos el hook de active company

--- src/features/users/hooks/useIndexUsers.tsx
+++ src/features/users/hooks/useIndexUsers.tsx
@@ -1,15 +1,18 @@
 import { IndexUserUseCase } from '@/application/use_cases/user/IndexUserUseCase';
 import { container } from '@/di/container';
 import { TYPES } from '@/di/types';
+import useActiveCompany from '@/features/companies/useActiveCompany';
 import { useQuery } from '@tanstack/react-query';
 
 export default function useIndexUser() {
 	const use_case = container.get<IndexUserUseCase>(TYPES.IndexUserUseCase);
+	const activeCompany = useActiveCompany();
 
 	const query = useQuery({
-		queryKey: ['users'],
+		queryKey: ['users', activeCompany?.id],
 		queryFn: () => use_case.execute(),
-		refetchOnWindowFocus: false
+		refetchOnWindowFocus: false,
+		placeholderData: (previousData) => previousData,
 		// staleTime: 5 * 60 * 1000,
 	});
 
